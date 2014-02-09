c71dad65c3c3f12e79cfb3509756017283be81b1
James Finlay <jtfinlay@ualberta.ca>
2014-02-09 02:06:40 -0700

    Twitter rows w/ placeholder

---
+++ GeerWeekApp/main/java/ca/ippetad/geerweekapp/model/Tweet.java
@@ -0,0 +1,12 @@
+package ca.ippetad.geerweekapp.model;
+
+/**
+ * Created by James on 2/9/14.
+ */
+public class Tweet {
+
+    public String name;
+    public String msg;
+    public String URL;
+    public String imgURL;
+}
--- GeerWeekApp/main/java/ca/ippetad/geerweekapp/view/FeedFragment.java
+++ GeerWeekApp/main/java/ca/ippetad/geerweekapp/view/FeedFragment.java
@@ -1,18 +1,27 @@
 package ca.ippetad.geerweekapp.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import ca.ippetad.geerweekapp.R;
+import ca.ippetad.geerweekapp.model.Tweet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by James on 2/8/14.
  */
 public class FeedFragment extends Fragment {
+    private static final String TAG = "FeedFragment";
 
     public FeedFragment() {}
 
@@ -22,11 +31,40 @@ public class FeedFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview);
-        listView.setAdapter(new ArrayAdapter(rootView.getContext(), android.R.layout.simple_list_item_1,
-                new String[]{"twitter feed here"}));
+        List<Tweet> tweets = new ArrayList<Tweet>();
+        Tweet t = new Tweet();
+        t.name = "Don Iveson";
+        t.msg = "I'll totally check out your hackathon! #yoloswag4jesus #compeclubfuckyeah";
+        tweets.add(t);
+        listView.setAdapter(new CustomArrayAdapter(rootView.getContext(), R.layout.twitter_row, tweets));
 
         return rootView;
     }
 
+    private class CustomArrayAdapter extends ArrayAdapter<Tweet> {
 
+        private List<Tweet> items;
+        private Context context;
+
+        public CustomArrayAdapter(Context context, int resource, List<Tweet> items) {
+            super(context, resource, items);
+            this.items = items;
+            this.context = context;
+        }
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View baseView = inflater.inflate(R.layout.twitter_row, parent, false);
+
+            /* Layout items */
+            ImageView profilePic = (ImageView) baseView.findViewById(R.id.profile_pic);
+            TextView txtName = (TextView) baseView.findViewById(R.id.txtName);
+            TextView txtSubject = (TextView) baseView.findViewById(R.id.txtMessage);
+
+            txtName.setText(items.get(position).name);
+            txtSubject.setText(items.get(position).msg);
+
+            return baseView;
+        }
+    }
 }
